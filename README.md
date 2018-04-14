ead9044ab8939026ae7dfa67ab37e079d413b6eb
Tony Gottschalk <tony.gottschalk2017@gmail.com>
2018-04-13 21:44:38 -0400

    Setup app instance on Heroku server and configured server.js
    Pushing updated server.js w/ Heroku instance.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Mongo-Scraper
-MongoDB Scraper
+UNC Bootcamp Homework
+
+Created during UNC Coding Bootcamp. This assignment was to create an online restaurant app using Node, Express, Handlebars, MySQL, a self-made ORM, adhere to MVC, and deploy on Heroku. 
+
+Technology used:
+
+* Node.js
+* Express
+* NPM
+* MySQL
+* Handlebars (NPM Package)
+
+Getting Started Heroku link:
+
+https://whispering-coast-33878.herokuapp.com/
---
+++ server.js
@@ -0,0 +1,28 @@
+(() => {
+	const bodyParser = require("body-parser");
+	const express = require("express");
+	const mongoose = require("mongoose");
+	const app = express();
+	const exphbs = require("express-handlebars");
+	const db = require("./models");
+	const PORT = process.env.PORT || 3000;
+	const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+
+	app.use(express.static(__dirname + "/public"));
+	require("./routes/html-routes.js")(app);
+	require("./routes/api-articles.js")(app);
+
+	app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+	app.set("view engine", "handlebars");
+
+	mongoose.Promise = Promise;
+	mongoose.connect(MONGODB_URI);
+
+	app.listen(PORT, function() {
+		console.log("App is running on port " + PORT + "!");
+	});
+
+})();
